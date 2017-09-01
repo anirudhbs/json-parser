3820588b226a142d0ef68f3a64f6f507c5004a5a
Anirudh B S <anirudhbs96@gmail.com>
2017-09-01 13:24:59 +0530

    removed unneeded braces

--- json-parser.js
+++ json-parser.js
@@ -5,18 +5,16 @@ const file = fs.readFileSync(path.join(__dirname,'./file.json')).toString()
 const numRE = /^([-+]?\d+\.?\d?([e][-+]?\d+)?)/
 const boolRE = /(^true|^false)/
 const spaceRE = /^(\s)+/
-let getParsed, temp
+let getParsed, temp= null
 
 const nullParser = function(input){
-    if(input.slice(0,4) != 'null')
-        return null
+    if(input.slice(0,4) != 'null') return null
     return [null, input.slice(4)]
 }
 
 const booleanParser = function(input){
     let bool = boolRE.exec(input)
-    if(!bool)
-        return null
+    if(!bool) return null
     if(bool[0] == 'true')
         return [true, input.slice(bool[0].length)]
     return [false, input.slice(bool[0].length)]
@@ -24,16 +22,14 @@ const booleanParser = function(input){
 
 const numberParser = function(input){
     let string = numRE.exec(input)
-    if(!string)
-        return null
+    if(!string) return null
     let number = parseInt(string[0])
     let length = string[0].length
     return [number, input.slice(length)]
 }
 
 const stringParser = function(input){
-    if (input[0] != '"')
-        return null
+    if (input[0] != '"') return null
     input = input.slice(1)
     let EoS = input.indexOf('"')
     let string = input.slice(0, EoS).toString()
@@ -42,32 +38,27 @@ const stringParser = function(input){
 
 const spaceParser = function(input){
     let temp = spaceRE.exec(input)
-    if(!temp)
-        return input
+    if(!temp) return input
     input = input.replace(spaceRE, '')
     return input
 }
 
 const commaParser = function(input){
     input = spaceParser(input)
-    if(input[0] != ',')
-        return null
+    if(input[0] != ',') return null
     return [',', input.slice(1)]
 }
 
 const arrayParser = function(input){
-    if(input[0] != '[')
-        return null
+    if(input[0] != '[') return null
     input = input.slice(1)
     let result, outputArray = []
     while(input[0] != ']'){
         result = valueParser(input) //value
         outputArray.push(result[0])
         input = result[1]
-
         result = commaParser(input) //comma
-        if(!result)
-            break
+        if(!result) break
         input = result[1]
     }
     return [outputArray, input.slice(1)]
@@ -75,20 +66,17 @@ const arrayParser = function(input){
 
 const colonParser = function(input){
     input = spaceParser(input)
-    if(input[0] != ':')
-        return null
+    if(input[0] != ':') return null
     return [':', input.slice(1)]
 }
 
 const objectParser = function(input){
-    if(input[0] != '{')
-        return null
+    if(input[0] != '{') return null
     input = input.slice(1)
     let outputObject = {}, result = null
     while(input[0] != '}'){
         input = spaceParser(input)
-        if(input[0] == '}')
-            break
+        if(input[0] == '}') break
         result = stringParser(input) //key
         let key = result[0]
         input = result[1]
@@ -99,8 +87,7 @@ const objectParser = function(input){
         input = result[1]
         outputObject[key] = value //store in object
         result = commaParser(input) //end of object
-        if(!result)
-            break
+        if(!result) break
         input = result[1]
     }
     return [outputObject, input.slice(1)]
@@ -127,10 +114,11 @@ const valueParser = function(input){
 
 try{
     getParsed = objectParser(file)
+    temp = JSON.stringify(getParsed[0], null, 4)
 }
 catch(err){
-    temp = 'Invalid input'
+    temp = 'Invalid JSON'
 }
 if(!temp)
-    temp = JSON.stringify(getParsed[0], null, 4)
+    temp = 'Invalid JSON'
 console.log(temp)
