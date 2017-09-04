be12e84d43f262ca8387bfeef0663f483dd9df62
Anirudh B S <anirudhbs96@gmail.com>
2017-09-04 09:34:42 +0530

    fixed string parser bug for escaped quotes

--- json-parser.js
+++ json-parser.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const file = fs.readFileSync(path.join(__dirname,'./file.json')).toString()
+const file = fs.readFileSync(path.join(__dirname,'./twitter.json')).toString()
 let temp = null
 
 const nullParser = function(input){
@@ -21,6 +21,7 @@ const numberParser = function(input){
 const stringParser = function(input){
     if (input[0] != '"') return null
     input = input.slice(1)
+    input = input.replace(/\\"/, '\'') //fix this
     let EoS = input.indexOf('"')
     let string = input.slice(0, EoS).toString()
     return [string, input.slice(EoS+1)]
@@ -43,8 +44,8 @@ const arrayParser = function(input){
         result = valueParser(input) //value
         outputArray.push(result[0])
         input = commaParser(spaceParser(result[1])) //comma
+        input = spaceParser(input) //testing
     }
-    input = spaceParser(input)
     return [outputArray, input.slice(1)]
 }
 
@@ -59,27 +60,20 @@ const colonParser = function(input){
 const objectParser = function(input){
     if(input[0] != '{') return null
     input = input.slice(1)
-
     let outputObject = {}, result = null
-
     while(input[0] != '}'){
         input = spaceParser(input)
         if(input[0] == '}') break
-
         result = keyParser(input) //key
         let key = result[0]
         input = result[1]
-        result = colonParser(spaceParser(input)) //colon
-        input = result
+        input = colonParser(spaceParser(input)) //colon
         result = valueParser(input) //value
         let value = result[0]
         input = result[1]
         outputObject[key] = value //store in object
-        result = commaParser(spaceParser(input))
-        //if(!result) break //end of object
-        input = result
+        input = commaParser(spaceParser(input))
     }
-    input = spaceParser(input)
     return [outputObject, input.slice(1)]
 }
 
@@ -88,24 +82,15 @@ const valueParser = function(input){
     let result
     if(result = (nullParser(input) || booleanParser(input) || stringParser(input) || numberParser(input) || arrayParser(input) || objectParser(input)))
         return result
-    //if (input != '')
-        return null
+    return null
 }
 
-let getParsed = valueParser(file)
-//temp = JSON.stringify(getParsed[0], null, 4)
-console.log(JSON.stringify(getParsed[0], null, 4))
-
-// map filter reduce
-/*
 try{
     let getParsed = valueParser(file)
     temp = JSON.stringify(getParsed[0], null, 4)
-}
-catch(err){
-    temp = 'Invalid JSON' //redundant
-}
-if(!temp)
+}catch(err){
     temp = 'Invalid JSON'
+}
+
+if(!temp) temp = 'Invalid JSON'
 console.log(temp)
-*/
